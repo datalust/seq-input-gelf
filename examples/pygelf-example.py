b3866dafe62955e96c18839de1216e64220112be
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2019-01-22 15:04:09 +1000

    Very simple pygelf example

---
+++ examples/pygelf-example.py
@@ -0,0 +1,11 @@
+# Example logging with https://github.com/keeprocking/pygelf
+# First, `pip install pygelf`
+
+from pygelf import GelfUdpHandler
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+logger.addHandler(GelfUdpHandler(host='127.0.0.1', port=12201))
+
+logger.info('Hello, Seq+GELF!')
