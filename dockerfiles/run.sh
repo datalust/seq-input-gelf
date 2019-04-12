66a15fd1288a8d02335085d9d104c50f4a7c92c7
Ashley Mannix <ashleymannix@live.com.au>
2019-04-12 12:12:43 +1000

    don't exec sqelf process and ignore invalid data

--- dockerfiles/run.sh
+++ dockerfiles/run.sh
@@ -6,4 +6,4 @@ if [ $SEQ_API_KEY ]; then
     api_key_arg="-a $SEQ_API_KEY"
 fi
 
-exec bin/sqelf | bin/seqcli/seqcli ingest --send-failure=continue --json -s $SEQ_ADDRESS $api_key_arg
+bin/sqelf | bin/seqcli/seqcli ingest --invalid-data=ignore --send-failure=continue --json -s $SEQ_ADDRESS $api_key_arg
