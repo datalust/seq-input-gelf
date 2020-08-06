fab15715ddf3a76d83af3beca46441aa380f0acb
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2020-08-06 16:01:21 +0200

    Add reading from SEQ_API_KEY_FILE

--- dockerfiles/run.sh
+++ dockerfiles/run.sh
@@ -3,6 +3,9 @@ set -eo pipefail
 
 # Add an API Key if specified
 api_key_arg=
+if [ -f "$SEQ_API_KEY_FILE" ]; then
+    SEQ_API_KEY=$(cat "$SEQ_API_KEY_FILE")
+fi
 if [ $SEQ_API_KEY ]; then
     api_key_arg="-a $SEQ_API_KEY"
 fi
