8e37aba3cc773011bd829f10fde2df40eb63ec93
Ashley Mannix <kodraus@hey.com>
2023-03-01 20:02:45 +1000

    add branch name to versions

--- ci/win-x64/build.ps1
+++ ci/win-x64/build.ps1
@@ -1,9 +1,14 @@
 param (
-  [string] $shortver = "99.99.99"
+  [string] $shortver = "99.99.99",
+  [string] $branch = "dev"
 )
 
 $ErrorActionPreference = "Stop"
 Push-Location "$PSScriptRoot/../../"
+
+if ($branch -ne "main") {
+    $shortver = "$shortver-$branch"
+}
 
 . "./ci/build-deps.ps1"
 
